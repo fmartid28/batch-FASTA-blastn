c97fc77bc4b5566a5d76ac1f859f8ebc20481d06
Fabio Martinez <fjmartinez94@gmail.com>
2019-10-31 23:18:00 -0400

    This update includes less necessary input from user. making it easier to use

--- batch_FASTA.sh
+++ batch_FASTA.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 chmod u+x batch_FASTA.sh
 
-
+##cd to directory containing batch_FASTA.sh code, if not already in PATH.
 ##moves all FASTAs for 16S querying into one folder
-echo 'Drag the main directory containing subdirectories with FASTAs of interest'
+echo 'Drag directory containing subdirectories with FASTAs of interest'
 read fasta_dir
 alias batch_FASTA=$fasta_dir
-echo 'Create a new folder to contain FASTA files and results and drag it here'
-read fasta_files
-find $fasta_dir -iname '*.fasta' -exec cp {} $fasta_files \;
+mkdir $fasta_dir/FASTA_files
+find $fasta_dir -iname '*.fasta' -exec cp {} $fasta_dir/FASTA_files \; 2> /dev/null
+	#2> /dev/null suppress shell script error message from cp FASTA in main directory to another file in a subdirectory
+echo -e '\n\nFASTAs ready for analysis\n\n'
 
-
-#loops query on each FASTA and produces a result for each FASTA
-#make sure blastn file is in same directory as 16S files
-echo drag folder containing 16SMicrobial db files
+##on mac make sure blastn file is in same directory as 16S files
+echo 'Drag folder containing 16SMicrobial database files'
 read dir_16S
-alias batch_FASTA=$dir_16S
 
-#actual l"oop code"
-for f in $fasta_files/*.fasta; do
-	echo $f;
+##looping FASTA files through blastn query command
+##for blastn to work, make sure 16SMicrobial DB is in 'current' directory. In loop below, it is made the current directory of the subshell by the alias command
+for f in $fasta_dir/FASTA_files/*.fasta; do
+	alias batch_FASTA=$dir_16S;
+	echo 'Processing' $(basename $f) '...';
 	b=$(basename -s .fasta $f);
-	"$dir_16S"/ncbi-blast-2.9.0+/bin/blastn -query "$f" -db 16SMicrobial -out "$fasta_files"/$b.doc -num_descriptions 5 -num_alignments 3;
+	'C:\Program Files\NCBI\blast-2.9.0+\bin\blastn' -query "$f" -db $dir_16S/16SMicrobial  -out $fasta_dir/FASTA_files/$b.doc -num_descriptions 5 -num_alignments 2;
+	echo -e '16S BLAST search completed\n';
 done
-
-
